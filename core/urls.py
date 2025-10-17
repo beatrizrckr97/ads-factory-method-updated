2f5a8f5600cdaaedbaca29b4daa820225528867b
Beatriz Roecker <152621606+beatrizrckr97@users.noreply.github.com>
2025-10-17 12:39:52 -0300

    Remove sales dashboard URL from urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,4 @@ from .views import home, generate_report, sales_dashboard_view
 urlpatterns = [
     path('', home, name='home'),
     path('report/<str:report_type>/', generate_report, name='generate_report'),
-    path('sales-dashboard/', sales_dashboard_view, name='sales_dashboard'),
 ]
