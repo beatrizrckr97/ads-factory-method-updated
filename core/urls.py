ff40ca9a2aa2531584ffb081d5c4e951ce39f796
beatrizrckr97 <152621606+beatrizrckr97@users.noreply.github.com>
2025-10-20 09:52:11 -0300

    Adiciona SalesReport e atualiza geração de relatórios

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from .views import home, generate_report, sales_dashboard_view
 urlpatterns = [
     path('', home, name='home'),
     path('report/<str:report_type>/', generate_report, name='generate_report'),
+    path('sales-dashboard/', sales_dashboard_view, name='sales_dashboard'),
 ]
