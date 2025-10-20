4f7d9ac528b45fb7ddb445d90fd5739d3bda26dc
julialg-220 <Tadsjulia@gmail.com>
2025-10-20 20:01:43 -0300

    criação do template de cliente e atualização do admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from .models import Transaction
+from .models import Client
+from .models import Sale
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
@@ -8,3 +10,39 @@ class TransactionAdmin(admin.ModelAdmin):
     search_fields = ('client_name',)
     list_filter = ('created_at',)
     ordering = ('-created_at',)
+
+
+
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+   
+    list_display = ('product_name', 'quantity', 'price', 'date')
+    
+   
+    list_filter = ('date', 'product_name')
+    
+   
+    search_fields = ('product_name',)
+    
+  
+    ordering = ('-date',)
+
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    
+    list_display = ('name', 'email', 'created_at')
+    
+  
+    list_filter = ('created_at',)
+    
+ 
+    search_fields = ('name', 'email')
+    
+   
+    ordering = ('-created_at',)
+    
+
+    readonly_fields = ('created_at',)
